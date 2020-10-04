8cab73f4d33b2975a79248499e99ae279deae407
zjustan <justinvandiggelen@me.com>
2020-10-04 17:07:11 +0200

    init
    added tree Editor(does nothing yet)
    added tree node
    added treesystem
    todo: add a easy to use tree editor

---
+++ Assets/progression-tree/TreeNode.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TreeNode : MonoBehaviour
+{
+    [Header("stats")]
+    public string skillname;
+
+    
+    public cost[] c;
+
+    public UnityEvent OnUnlock, OnMadeAvaible;
+    public bool starternode;
+    [Space()]
+    [Header("meta")]
+    public TreeNode parentNode;
+    public TreeNode[] childNodes;
+    Button button;
+    Image img;
+    bool unlocked, avaible;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        img = GetComponent< Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (starternode)
+        {
+            MakeAvaible();
+        }
+        else
+        {
+            button.interactable = false;
+        }
+    }
+
+
+    public void MakeAvaible()
+    {
+        avaible = true;
+        button.interactable = true;
+    }
+
+    public void OnClick()
+    {
+
+        unlocked = true;
+        OnUnlock.Invoke();
+        button.interactable = false;
+        foreach(TreeNode treeNode in childNodes)
+        {
+            treeNode.MakeAvaible();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!starternode)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, parentNode.transform.position);
+        }
+
+        Gizmos.color = Color.blue;
+        foreach (TreeNode treeNode in childNodes)
+        {
+            Gizmos.DrawLine(transform.position, treeNode.transform.position);
+        }
+
+    }
+
+    [System.Serializable]
+    public struct cost
+    {
+        public resource resource;
+        public int amount;
+        public bool removeOnUnlock;
+    }
+
+    public enum resource
+    {
+        energy,
+        population
+    }
+
+}
---
+++ Assets/progression-tree/TreeSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TreeSystem : MonoBehaviour
+{
+    public static TreeSystem instance;
+    public UnityEvent<TreeNode> unlock;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("instance already exist, destroying object");
+            Destroy(this);
+        }
+    }
+
+
+}
