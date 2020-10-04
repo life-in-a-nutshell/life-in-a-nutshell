836ed5cf507bf92fa66e7aaed26f4226f3e2654c
MrSawce <ascanio_b@hotmail.com>
2020-10-04 21:14:30 +0200

    Cutting trees destroys object

---
+++ Assets/TreeLog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeLog : MonoBehaviour
+{
+    public GameObject treeSawedPrefab;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D col)
+    {        
+        if (col.tag == "Chainsaw")
+        {
+            Instantiate(treeSawedPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
