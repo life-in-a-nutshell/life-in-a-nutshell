2e9e75689be3f2e9c0171e5598dc51e6d67f680b
ASCANIO-WS\Ascanio <ascanio.bajada.a106234@mcast.edu.mt>
2020-10-06 00:29:09 +0200

    Added Spawner and objects destroyed after a short amount of time.
    Future Improvements:
    -Spawners are static, need to create procedurally generated spawners.
    -Treelogs need to have a rotation applied to them based on the rotation the Tree was sawed,
    -Add animals in spawner which the user must avoid.
    -More trees variety
    -Time or score limit
    -UI

---
+++ Assets/TreeSpawnerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawnerScript : MonoBehaviour
+{
+
+    public GameObject treePrefab;
+    public Transform[] spawnPoints;
+
+    public float minDelay = .1f;
+    public float maxDelay = 1f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTrees());
+    }
+
+    IEnumerator SpawnTrees()
+    {
+        while(true)
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            yield return new WaitForSeconds(delay);
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            GameObject spawnedTree = Instantiate(treePrefab, spawnPoint.position, spawnPoint.rotation);
+            Destroy(spawnedTree, 5f);
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
