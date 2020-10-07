3fd2fbd7379f97d553512f6e53b3a92a269dccf7
IPalos <ignaciopalos.r@gmail.com>
2020-10-06 20:36:38 -0500

    Add a tile highlight cursor (#6)
    * Add a highlight cursor to show where to build and checks if that tile its available fo building
    
    * Adds right-click canceling, and disables building placement button when cursor is active
    
    * Declared Player Variables + get/set (#10)
    
    * Add a highlight cursor to show where to build and checks if that tile its available fo building
    
    * Adds right-click canceling, and disables building placement button when cursor is active
    
    * remove vscode settings and rebased to new master
    
    * Remove vscode directory
    
    * add vscode directory to gitignore
    
    Co-authored-by: OStyxO <44876527+OStyxO@users.noreply.github.com>

---
+++ Assets/Scripts/CursorTileScripts/CursorTileController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+public class CursorTileController : MonoBehaviour
+{
+
+	[SerializeField]
+	Tile availableTile;
+	[SerializeField]
+	Tile unavailableTile;
+
+	[SerializeField]
+	Tilemap terrainTilemap;
+
+	[SerializeField]
+	Tilemap buildingsTilemap;
+
+	[SerializeField]
+	Tilemap overlayTilemap;
+	Camera cam;
+
+	bool buildingBeingPlaced;
+
+	[SerializeField]
+	Tile unbuildableTile;
+	[SerializeField]
+	Tile buildingTile;
+
+
+
+	void Start()
+	{
+		cam = Camera.main;
+		buildingBeingPlaced = false;
+		GameEvents.current.onEnableTileCursor += EnableTileCursor;
+		GameEvents.current.onDisableTileCursor += DisableTileCursor;
+	}
+
+	void Update()
+	{
+		if (buildingBeingPlaced)
+		{
+			overlayTilemap.ClearAllTiles();
+			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+			Vector3Int terrainPosition = terrainTilemap.WorldToCell(cam.ScreenToWorldPoint(mousePos));
+			Vector3Int buildingPosition = buildingsTilemap.WorldToCell(cam.ScreenToWorldPoint(mousePos)) + new Vector3Int(0, 0, -1);
+			Vector3Int gridPosition = overlayTilemap.WorldToCell(cam.ScreenToWorldPoint(mousePos));
+			Vector3Int overlayPosition = gridPosition + new Vector3Int(0, 0, -2);
+			if (terrainTilemap.HasTile(terrainPosition))
+			{
+				Debug.Log(terrainTilemap.GetTile(gridPosition));
+				if (terrainTilemap.GetTile(terrainPosition) != unbuildableTile && !buildingsTilemap.HasTile(buildingPosition))
+				{
+					overlayTilemap.SetTile(overlayPosition, availableTile);
+					if (Input.GetMouseButtonDown(0))
+					{
+						buildingsTilemap.SetTile(buildingPosition, buildingTile);
+						GameEvents.current.DisableTileCursor();
+						buildingBeingPlaced = false;
+
+					}
+
+
+				}
+				else
+				{
+					overlayTilemap.SetTile(overlayPosition, unavailableTile);
+
+				}
+
+			}
+			if (Input.GetMouseButtonDown(1))
+			{
+				buildingBeingPlaced = false;
+				GameEvents.current.DisableTileCursor();
+			}
+
+		}
+	}
+	void EnableTileCursor()
+	{
+		buildingBeingPlaced = true;
+	}
+
+	void DisableTileCursor()
+	{
+		buildingBeingPlaced = false;
+		overlayTilemap.ClearAllTiles();
+
+	}
+
+}
---
+++ Assets/Scripts/CursorTileScripts/PlaceBuildingButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class PlaceBuildingButton : MonoBehaviour
+{
+
+	bool isButtonEnabled;
+	public TextMeshProUGUI buttonText;
+	// Start is called before the first frame update
+	void Start()
+	{
+		isButtonEnabled = true;
+		GameEvents.current.onDisableTileCursor += EnableBuildButton;
+		GameEvents.current.onEnableTileCursor += DisableBuildButton;
+
+	}
+
+	private void OnGUI()
+	{
+		if (isButtonEnabled)
+		{
+			buttonText.text = "Building placement is OFF";
+		}
+		else
+		{
+			buttonText.text = "Building placement is ON";
+		}
+	}
+
+	void EnableBuildButton()
+	{
+		isButtonEnabled = true;
+		GetComponent<Button>().interactable = true;
+
+	}
+
+	void DisableBuildButton()
+	{
+		isButtonEnabled = false;
+		GetComponent<Button>().interactable = false;
+	}
+}
